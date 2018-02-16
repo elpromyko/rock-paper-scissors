4dc719a339f85f0804cb104ea1bd79081155ad35
Piotr Prominski <piotr.prominski@gmail.com>
2018-02-16 18:24:15 +0100

    edit readme.md, compact methods, cosmetics in xml, fix onRestoreInstanceState method

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # rock-paper-scissors
-Rock Paper Scissors game app - 2nd project for Google / Udacity Scholarship Program - Android Basics Course
+Twist on a Rock Paper Scissors game app - 2nd project for Google / Udacity Scholarship Program - Android Basics Course
 
 1 ![rock_paper_scissors_screen](https://github.com/elpromyko/img/blob/master/rps2.png) 2 ![rock_paper_scissors_screen](https://github.com/elpromyko/img/blob/master/fig2re.png) 3 ![rock_paper_scissors_screen](https://github.com/elpromyko/img/blob/master/fig3re.png)
 
@@ -9,7 +9,9 @@ Rules:
 2) All the buttons on player side become grayed out (disabled) fig.2.
 3) Pass a phone to your opponent.
 4) Repeat 1) and 2)
-5) Click Result button to display players choices, points are added if someone wins according to following rules fig.3:
+5) Click Result button to display players choices, points are added / deducted according to the following rules fig.3:
+ * every attack has points value that is added / deducted to / from final score depends on a result
+ * scissors (+/- 3pts), rock (+/- 2pts), paper (+/- 1pt)
  * rock beats scissors
  * scissors beats paper
  * paper beats rock
--- app/src/main/java/com/example/android/rockpaperscissors/MainActivity.java
+++ app/src/main/java/com/example/android/rockpaperscissors/MainActivity.java
@@ -10,21 +10,19 @@ import java.util.Hashtable;
 
 public class MainActivity extends AppCompatActivity {
 
-    String player1Attack;
-    String player2Attack;
-    Button btn1;
-    Button btn2;
-    Button btn3;
-    Button btn4;
-    Button btn5;
-    Button btn6;
-    int scorePlayer1 = 0;
-    int scorePlayer2 = 0;
     static final String ROCK = "Rock";
     static final String PAPER = "Paper";
     static final String SCISSORS = "Scissors";
     static final String PLAYER1_SCORE = "player1Score";
     static final String PLAYER2_SCORE = "player2Score";
+    static final String PLAYER1_ATTACK = "player1Attack";
+    static final String PLAYER2_ATTACK = "player2Attack";
+    static final String RESULT_BTN_STATE = "resultBtnState";
+    static final String PLAYER1_BTNS_STATE = "player1BtnsState";
+    static final String PLAYER2_BTNS_STATE = "player2BtnsState";
+    String player1Attack, player2Attack;
+    Button btn1, btn2, btn3, btn4, btn5, btn6, resultBtn;
+    int scorePlayer1, scorePlayer2;
     Hashtable<String, Integer> attackValues = new Hashtable<>();
 
     @Override
@@ -37,69 +35,73 @@ public class MainActivity extends AppCompatActivity {
         btn4 = findViewById(R.id.button_4);
         btn5 = findViewById(R.id.button_5);
         btn6 = findViewById(R.id.button_6);
+        resultBtn = findViewById(R.id.result_button);
         player1Attack = getString(R.string.fight);
         player2Attack = getString(R.string.fight);
         scorePlayer1 = Integer.parseInt(getString(R.string.player_1_score));
         scorePlayer2 = Integer.parseInt(getString(R.string.player_2_score));
-        attackValues.put("Scissors", 3);
-        attackValues.put("Rock", 2);
-        attackValues.put("Paper", 1);
+        attackValues.put(SCISSORS, 3);
+        attackValues.put(ROCK, 2);
+        attackValues.put(PAPER, 1);
     }
 
     @Override
     public void onSaveInstanceState(Bundle savedInstanceState) {
         savedInstanceState.putInt(PLAYER1_SCORE, scorePlayer1);
         savedInstanceState.putInt(PLAYER2_SCORE, scorePlayer2);
+        savedInstanceState.putBoolean(PLAYER1_BTNS_STATE, btn1.isEnabled());
+        savedInstanceState.putBoolean(PLAYER2_BTNS_STATE, btn4.isEnabled());
+        savedInstanceState.putBoolean(RESULT_BTN_STATE, resultBtn.isEnabled());
+        savedInstanceState.putString(PLAYER1_ATTACK, player1Attack);
+        savedInstanceState.putString(PLAYER2_ATTACK, player2Attack);
         super.onSaveInstanceState(savedInstanceState);
     }
 
     public void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
 
-        // Restore score states from saved instance and display them
+        // Restore score, attack and button states from saved instance and display them
         scorePlayer1 = savedInstanceState.getInt(PLAYER1_SCORE);
         scorePlayer2 = savedInstanceState.getInt(PLAYER2_SCORE);
-        displayPlayer1Score(scorePlayer1);
-        displayPlayer2Score(scorePlayer2);
+        player1Attack = savedInstanceState.getString(PLAYER1_ATTACK);
+        player2Attack = savedInstanceState.getString(PLAYER2_ATTACK);
+        displayPlayersScores(scorePlayer1, scorePlayer2);
+        displayPlayersAttacks(player1Attack, player2Attack);
+        setButtonsStateForPlayer1(savedInstanceState.getBoolean(PLAYER1_BTNS_STATE));
+        setButtonsStateForPlayer2(savedInstanceState.getBoolean(PLAYER2_BTNS_STATE));
+        resultBtn.setEnabled(savedInstanceState.getBoolean(RESULT_BTN_STATE));
     }
 
-    public void displayPlayer1Attack(String WeaponPlayer1) {
-        TextView ScoreTeamAView = findViewById(R.id.player1_weapon);
-        ScoreTeamAView.setText(String.valueOf(WeaponPlayer1));
+    public void displayPlayersAttacks(String WeaponPlayer1, String WeaponPlayer2) {
+        TextView ScorePLayer1View = findViewById(R.id.player1_weapon);
+        TextView ScorePlayer2View = findViewById(R.id.player2_weapon);
+        ScorePLayer1View.setText(String.valueOf(WeaponPlayer1));
+        ScorePlayer2View.setText(String.valueOf(WeaponPlayer2));
     }
 
-    public void displayPlayer2Attack(String WeaponPlayer2) {
-        TextView ScoreTeamAView = findViewById(R.id.player2_weapon);
-        ScoreTeamAView.setText(String.valueOf(WeaponPlayer2));
+    public void setButtonsStateForPlayer1(boolean state) {
+        btn1.setEnabled(state);
+        btn2.setEnabled(state);
+        btn3.setEnabled(state);
     }
 
-    public void deactivateAllButtonsForPlayer1() {
-        btn1.setEnabled(false);
-        btn2.setEnabled(false);
-        btn3.setEnabled(false);
-    }
-
-    public void deactivateAllButtonsForPlayer2() {
-        btn4.setEnabled(false);
-        btn5.setEnabled(false);
-        btn6.setEnabled(false);
+    public void setButtonsStateForPlayer2(boolean state) {
+        btn4.setEnabled(state);
+        btn5.setEnabled(state);
+        btn6.setEnabled(state);
     }
 
     public void activateAllButtons(View view) {
-        btn1.setEnabled(true);
-        btn2.setEnabled(true);
-        btn3.setEnabled(true);
-        btn4.setEnabled(true);
-        btn5.setEnabled(true);
-        btn6.setEnabled(true);
+        setButtonsStateForPlayer1(true);
+        setButtonsStateForPlayer2(true);
+        resultBtn.setEnabled(true);
         resetWeapons();
-        displayPlayer1Attack(player1Attack);
-        displayPlayer2Attack(player2Attack);
+        displayPlayersAttacks(player1Attack, player2Attack);
     }
 
     public void resetWeapons() {
-        player1Attack = "Fight!";
-        player2Attack = "Fight!";
+        player1Attack = getString(R.string.fight);
+        player2Attack = getString(R.string.fight);
     }
 
     public void resetScores() {
@@ -110,51 +112,44 @@ public class MainActivity extends AppCompatActivity {
     public void reset(View view) {
         activateAllButtons(null);
         resetScores();
-        displayPlayer1Score(scorePlayer1);
-        displayPlayer2Score(scorePlayer2);
+        displayPlayersScores(scorePlayer1, scorePlayer2);
     }
 
-    public void setPlayer1Attack (View view) {
+    public void setPlayer1Attack(View view) {
         Button btn = (Button) view;
         player1Attack = btn.getText().toString();
-        deactivateAllButtonsForPlayer1();
+        setButtonsStateForPlayer1(false);
     }
 
-    public void setPlayer2Attack (View view) {
+    public void setPlayer2Attack(View view) {
         Button btn = (Button) view;
         player2Attack = btn.getText().toString();
-        deactivateAllButtonsForPlayer2();
+        setButtonsStateForPlayer2(false);
     }
 
-    public void displayPlayer1Score(int player1Score) {
-        TextView ScoreTeamAView = findViewById(R.id.player1_score);
-        ScoreTeamAView.setText(String.valueOf(player1Score));
-    }
-
-    public void displayPlayer2Score(int player2Score) {
-        TextView ScoreTeamAView = findViewById(R.id.player2_score);
-        ScoreTeamAView.setText(String.valueOf(player2Score));
+    public void displayPlayersScores(int player1Score, int player2Score) {
+        TextView ScorePlayer1View = findViewById(R.id.player1_score);
+        TextView ScorePlayer2View = findViewById(R.id.player2_score);
+        ScorePlayer1View.setText(String.valueOf(player1Score));
+        ScorePlayer2View.setText(String.valueOf(player2Score));
     }
 
     public void addScore(View view) {
-        if ((! player1Attack.equals(player2Attack))
-                && ((! player1Attack.equals("Fight!")) && (! player2Attack.equals("Fight!")) )) {
+        if ((!player1Attack.equals(player2Attack))
+                && ((!player1Attack.equals("Fight!")) && (!player2Attack.equals("Fight!")))) {
             if ((player1Attack.equals(ROCK) && player2Attack.equals(SCISSORS))
                     || (player1Attack.equals(PAPER) && player2Attack.equals(ROCK))
                     || (player1Attack.equals(SCISSORS) && player2Attack.equals(PAPER))) {
                 scorePlayer1 += attackValues.get(player1Attack);
                 scorePlayer2 -= attackValues.get(player2Attack);
-                displayPlayer1Score(scorePlayer1);
-                displayPlayer2Score(scorePlayer2);
+                displayPlayersScores(scorePlayer1, scorePlayer2);
             } else {
                 scorePlayer1 -= attackValues.get(player1Attack);
                 scorePlayer2 += attackValues.get(player2Attack);
-                displayPlayer1Score(scorePlayer1);
-                displayPlayer2Score(scorePlayer2);
+                displayPlayersScores(scorePlayer1, scorePlayer2);
             }
         }
-        displayPlayer1Attack(player1Attack);
-        displayPlayer2Attack(player2Attack);
-        resetWeapons();
+        displayPlayersAttacks(player1Attack, player2Attack);
+        resultBtn.setEnabled(false);
     }
 }
